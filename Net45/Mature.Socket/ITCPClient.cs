7b01d84f34803b037707098c7ceee87df0cb963d
fengjuanchensha <1130217572@qq.com>
2020-12-28 16:16:57 +0800

    删除注释掉的代码
    删除注释掉的代码

--- Net45/Mature.Socket/ITCPClient.cs
+++ Net45/Mature.Socket/ITCPClient.cs
@@ -10,7 +10,6 @@ namespace Mature.Socket
         string SessionId { get; }
         EndPoint RemoteEndPoint { get; }
         Task<bool> ConnectAsync(string ip, ushort port);
-        //Task<string> SendAsync(string key, string body, int timeout);
         Task<TResponse> SendAsync<TRequest, TResponse>(string key, TRequest request, int timeout);
         void RegisterNotify<TResponse>(string key, Action<TResponse> action);
         void UnRegisterNotify<TResponse>(string key, Action<TResponse> action);
